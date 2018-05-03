eb55aad21272437149ccabc341450c12d0185d09
susan <susan@disroot.org>
2018-05-03 23:33:31 +0100

    1s proportion checked

--- python/gen.py
+++ python/gen.py
@@ -1,4 +1,4 @@
-import scipy
+import scipy.stats
 import numpy as np
 
 def pcauchy(x, location, scale, shape):
@@ -9,13 +9,15 @@ def pcauchy(x, location, scale, shape):
 def genpcauchy(n, betavec, shape):
     beta0 = betavec[0]
     beta = betavec[1:]
-    x1 = scipy.stats.uniform.rvs(size=n)
+    x1 = scipy.stats.uniform.rvs(size=n, loc=-2, scale=4)
     x2 = scipy.stats.norm.rvs(size=n)
     X = np.column_stack((x1,x2))
     prob = pcauchy(beta0 + np.dot(beta,X.T), 0, 1, shape)
     y = scipy.stats.bernoulli.rvs(p = prob)
     return np.column_stack((X,y))
 
+dat = genpcauchy(1000, np.array([-3.7,-0.51,11.2]), shape = 2.6)
+dat.mean(axis =0)
 
 
 
